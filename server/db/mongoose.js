db69b0fbcce9efe79f2628f77acd1e5fe6b1d047
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2019-01-16 18:19:55 +0100

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,10 @@ mongoose.Promise = global.Promise;
 // mongoose.connect(prescess.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 
+// process.env.NODE_ENV === 'production'
+// process.env.NODE_ENV === 'development'
+// process.env.NODE_ENV === 'test'
+
 mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 
