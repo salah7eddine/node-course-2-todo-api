f63eb78dc724629e78e50c882693d634e6d11344
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2019-01-16 08:46:32 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -22,29 +24,56 @@ app.post('/user', (req, res) => {
   });
 });
 
-// app.post('/todos', (req, res) => {
-//   //console.log(req.body);
-//   var todo = new Todo({
-//     text: req.body.text
-//   });
+app.post('/todos', (req, res) => {
+  //console.log(req.body);
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-//   todo.save().then((doc) => {
-//     res.send(doc);
-//   }, (e) => {
-//     res.status(400).send(e);
-//   });
-// });
-
-
-// app.get('/todos', (req,res) => {
-//   Todo.find().then((todos) => {
-//     res.send({todos});
-//   }, (e) => {
-//     res.status(400).send(e);
-//   });
-// });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+// GET /todos:123123
+
+// Valid id using isValid
+  // 404 -send back empty body
+app.get('/todos/:id', (req, res) =>{
+  var id = req.params.id;
+  
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send({});
+  }
+
+  // findById 
+    // success
+      // if todod - send it back
+      // if no todo - send back 404 with empyu body
+    // error
+      // 400 - and send empty body back
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.send('send back 404 with empty body');
+    }
+    res.send({todo});
+  }).catch((e) => console.log(e));
+    
+
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
